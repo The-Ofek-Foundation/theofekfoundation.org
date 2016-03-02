0645895a858cd48da22b472de95309ac6fc5eedd
Ofek Gila <flamingo.ofek@gmail.com>
2016-03-01 17:31:17 -0800

    Append / to game urls in code

--- games/views.py
+++ games/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 def connectfour(request):
 	page = {
-		'full_url': 'http://theofekfoundation.org/games/ConnectFour',
+		'full_url': 'http://theofekfoundation.org/games/ConnectFour/',
 		'full_description': "Connect Four online Monte Carlo AI! Play against human or strong computer! Save your games easily!",
 		'description': "Connect Four online Monte Carlo AI!",
 		'title': 'Connect Ofek',
@@ -14,7 +14,7 @@ def connectfour(request):
 
 def weiqi(request):
 	page = {
-		'full_url': 'http://theofekfoundation.org/games/OnlineGo',
+		'full_url': 'http://theofekfoundation.org/games/OnlineGo/',
 		'full_description': "The online games of Go (Weiqi), Gomoku (with a minimax AI), and Wu, with a strong minimax AI for Gomoku.",
 		'description': "Play games in the Go family online!",
 		'title': 'We iqi',
