5e54506dd379ab24a389f482be7c9ed68376c7c3
Ofek Gila <flamingo.ofek@gmail.com>
2016-05-04 17:32:25 -0700

    change from sqlite3 to mysql

--- TheOfekFoundation/settings.py
+++ TheOfekFoundation/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'TheOfekFoundation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ofekih$theofekfoundationdb',
         'USER': 'ofekih',
         'PASSWORD': 'SFD#$%\36grdsg%&$t',
