85b0bdaba6db7a008b41640df4c2d93f569371df
Ofek Gila <ofek@theofekfoundation.org>
2016-10-19 16:49:37 -0700

    Serve blog over https

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import HttpResponseRedirect
 from django.conf import settings
 
 def blog(request, pathname):
-	return HttpResponseRedirect("http://blog." + settings.DOMAIN + "/" + pathname)
+	return HttpResponseRedirect("https://blog." + settings.DOMAIN + "/" + pathname)
