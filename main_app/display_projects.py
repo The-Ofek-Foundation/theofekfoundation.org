656fb5df85f459ff2cc3e2ca06c4bceb47bdb6d6
Ofek Gila <ofek@theofekfoundation.org>
2017-10-01 02:15:29 -0700

    Make website more mobile friendly

--- main_app/display_projects.py
+++ main_app/display_projects.py
@@ -12,6 +12,7 @@ games = {
 			'template': 'games/ConnectFourDescription.html',
 			'save': True,
 			'display_main': True,
+			'mobile_friendly': True,
 		},	{
 			'name': 'Ultimate Tic Tac Toe',
 			'imagesrc': 'Ultimate-Tic-Tac-Toe.jpg',
@@ -19,6 +20,7 @@ games = {
 			'template': 'games/UltimateTicTacToeDescription.html',
 			'save': True,
 			'display_main': True,
+			'mobile_friendly': True,
 		},	{
 			'name': 'Old Snakey',
 			'imagesrc': 'snakey_level_1.gif',
@@ -26,6 +28,7 @@ games = {
 			'template': 'games/OldSnakeyDescription.html',
 			'save': True,
 			'display_main': True,
+			'mobile_friendly': False,
 		},	{
 			'name': 'Weiqi',
 			'imagesrc': 'board-game-go.jpg',
@@ -33,6 +36,7 @@ games = {
 			'template': 'games/WeiqiDescription.html',
 			'save': False,
 			'display_main': False,
+			'mobile_friendly': True,
 		},	{
 			'name': 'Mancala',
 			'imagesrc': 'mancala.JPG',
@@ -40,6 +44,7 @@ games = {
 			'template': 'games/MancalaDescription.html',
 			'save': True,
 			'display_main': True,
+			'mobile_friendly': True,
 		},	{
 			'name': 'Lame Duck',
 			'imagesrc': 'duck.svg',
@@ -47,18 +52,21 @@ games = {
 			'template': 'games/LameDuckDescription.html',
 			'save': False,
 			'display_main': True,
+			'mobile_friendly': False,
 		},	{
 			'name': 'Minesweeper',
 			'description': "Simple Minesweeper with multiple difficulties!",
 			'template': 'games/MinesweeperDescription.html',
 			'save': True,
 			'display_main': False,
+			'mobile_friendly': True,
 		},	{
 			'name': u'מפגשים צולבים',
 			'description': "Play the Israeli מפגשים צולבים game online against an AI!",
 			'template': 'games/CrossroadsDescription.html',
 			'save': True,
 			'display_main': False,
+			'mobile_friendly': True,
 		},
 	],
 }
@@ -72,11 +80,13 @@ tools = {
 			'imagesrc': 'maze.png',
 			'description': 'A <a class="animated" href="https://en.wikipedia.org/wiki/Maze_generation_algorithm#Depth-first_search">depth-first search maze generator</a> with a generation <strong>animation option</strong> and maze styles!',
 			'display_main': True,
+			'mobile_friendly': True,
 		},	{
 			'name': 'Prime Factorizer',
 			'imagesrc': 'factor-tree.gif',
 			'description': 'A super-fast prime number factorizer that uses a sieve of a size that you get to input!',
 			'display_main': True,
+			'mobile_friendly': True,
 		},	{
 		# 	'name': 'Image Editor',
 		# 	'imagesrc': 'image-editor.jpg',
@@ -86,21 +96,25 @@ tools = {
 			'imagesrc': 'grapher.gif',
 			'description': 'A function grapher with a neat tracing and newtons\' method (click \'n\' in trace mode).',
 			'display_main': True,
+			'mobile_friendly': True,
 		},	{
 			'name': 'Happy Number Calculator',
 			'imagesrc': 'happy-number.png',
 			'description': 'A <a class="animated" href="https://en.wikipedia.org/wiki/Happy_number">Happy Number</a> calculator.',
 			'display_main': True,
+			'mobile_friendly': True,
 		},	{
 			'name': 'Text Reverser',
 			'imagesrc': '4600275420_c7bfb8466d.jpg',
 			'description': 'Reverse the last two characters of words :P',
 			'display_main': True,
+			'mobile_friendly': True,
 		},	{
 			'name': 'Password Generator',
 			'imagesrc': 'password.jpg',
 			'description': 'Generate random passwords quickly!',
 			'display_main': True,
+			'mobile_friendly': True,
 		},
 	],
 }
